b9afef9d9bcc1eddccfdde9612fe42708aab61ed
gils0 <102681625+gils0@users.noreply.github.com>
2022-03-30 23:29:01 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Login page</title>
+</head>
+<body>
+
+    <p>Login</p>
+
+<form>
+    <input type="text" id="user" name="username" placeholder="username"/>
+
+</body>
+</html>
